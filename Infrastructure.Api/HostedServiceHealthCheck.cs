2679f2f35438e6cfdb8ce3575f239f7754c21c24
Dmitry Valeev <valeev.diman@gmail.com>
2020-04-06 22:01:15 +0400

    Hosted service + hc for it

---
+++ Infrastructure.Api/HostedServiceHealthCheck.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Infrastructure.Api
+{
+    /// <summary>
+    /// Self hosted services health check
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public class HostedServiceHealthCheck : IHealthCheck
+    {
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Name => "slow_dependency_check";
+
+        /// <summary>
+        /// List of background services and their status
+        /// </summary>
+        public Dictionary<string, bool> BackgroundServices { get; set; } = new Dictionary<string, bool>();
+
+        /// <summary>
+        /// Check is process is ended
+        /// </summary>
+        /// <returns></returns>
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (BackgroundServices.All(t => t.Value))
+            {
+                return Task.FromResult(HealthCheckResult.Healthy("The background services are still running"));
+            }
+            return Task.FromResult(HealthCheckResult.Unhealthy("One or all background services are not running"));
+        }
+    }
+}
