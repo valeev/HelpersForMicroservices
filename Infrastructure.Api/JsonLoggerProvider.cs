2df4997ca77241cc6423773e9ed516f1cea8435d
Valeev Dmitry <valeev.diman@gmail.com>
2021-06-17 11:24:00 +0400

    Custom JsonLogger with excluding duplicated state details

---
+++ Infrastructure.Api/JsonLoggerProvider.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
+
+namespace Infrastructure.Api
+{
+    public sealed class JsonLoggerProvider : ILoggerProvider
+    {
+        private readonly LoggerExternalScopeProvider _scopeProvider = new LoggerExternalScopeProvider();
+        private readonly ConcurrentDictionary<string, JsonLogger> _loggers = new ConcurrentDictionary<string, JsonLogger>(StringComparer.Ordinal);
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return _loggers.GetOrAdd(categoryName, category => new JsonLogger(Console.Out, category, _scopeProvider));
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
